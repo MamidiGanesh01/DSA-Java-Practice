809a318ab777547af9f8e18d1ed83e5ae94fc4e0
GANESH MAMIDI <mamidiganesh114@gmail.com>
2026-02-02 23:08:12 +0530

    Day 2 – Move Zeroes (Two Pointers)

---
+++ Arrays/MoveZeroes.java
@@ -0,0 +1,21 @@
+/*
+Problem: Move Zeroes (LeetCode)
+Approach: Two Pointers (Swap non-zero elements forward)
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
